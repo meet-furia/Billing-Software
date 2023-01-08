a4bded345eb2740c1dc98dde643d7331cd06efce
Meet Furia <furia.meet@gmail.com>
2023-01-08 16:37:20 +0530

    Added Login, Registration and Admin Panel Functionality

---
+++ src/billing_software/BillingSoftware.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package billing_software;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author india
+ */
+public class BillingSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+        new StartProject().setVisible(true);
+ //       new Login().setVisible(true);
+    }
+    
+}
